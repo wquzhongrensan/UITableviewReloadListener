7fdd8731e4f3ba1bea52839a48880b8d24fa34f5
jianpingli <956023769@qq.com>
2017-02-06 13:41:26 +0800

    Initial Commit

---
+++ TableviewReloadListener/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableviewReloadListener
+//
+//  Created by AppsComm on 2017/2/6.
+//  Copyright © 2017年 quzhongrensan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableviewReloadListener/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableviewReloadListener
+//
+//  Created by AppsComm on 2017/2/6.
+//  Copyright © 2017年 quzhongrensan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
